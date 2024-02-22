397a905d736bec88dca10ee0babba373f51aaf3e
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2024-02-22 10:34:50 +0100

    Second Commit

--- de_Slutprojekt/Program.cs
+++ de_Slutprojekt/Program.cs
@@ -1,3 +1,22 @@
 ﻿using Raylib_cs;
 
+int display = Raylib.GetCurrentMonitor();    //get monitor display
 
+const int screenWidth = 800;                 //original window size
+const int screenHeight = 450;                //original window size
+
+int monitordisplay = Raylib.GetCurrentMonitor();    //get monitor display
+
+Raylib.InitWindow(screenWidth, screenHeight, "VidoGam");    //set window size
+Raylib.SetTargetFPS(60);                                   //set target fps for the window
+
+Texture2D PixelDojo = Raylib.LoadTexture("PixelDojo.png");
+
+int currentRoom = 0;
+
+while (!Raylib.WindowShouldClose())
+{
+
+    if (currentRoom == 0)
+    { }
+}
